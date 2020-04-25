e9d44767f88453db5b7a32c994de54b1a5cca7a9
Soufiene Slimi <soufiene.slimi@mail.com>
2020-04-25 18:20:00 +0100

    :package: packaing the field

---
+++ src/AddonServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace SoufieneSlimi\StarFieldForBackpack;
+
+use Illuminate\Support\ServiceProvider;
+
+class AddonServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(realpath(__DIR__.'/resources/views'), 'star-field-for-backpack');
+    }
+}
